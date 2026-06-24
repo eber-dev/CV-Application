122e9aaf3ef1fd5a7df107d909426a73c6920d3c
Eber <www.eberacunaquispe@gmail.com>
2026-06-24 15:16:00 -0500

    Se añadio renderizado condicional y se cambia el estado para saber si fue llenado o no el formulario tambien se permite editar

--- src/components/general/seccion1.jsx
+++ src/components/general/seccion1.jsx
@@ -11,6 +11,7 @@ function General() {
 
     const enviado = (e) => {
         e.preventDefault();
+        setMostrarInfo(true);
     };
 
     const handleChange = (e) => {
@@ -21,52 +22,78 @@ function General() {
         });
     };
 
+    const [mostrarInfo, setMostrarInfo] = useState(false);
+
+    const handleEditar = () => {
+        setMostrarInfo(false);
+    };
+
     return (
         <div>
             <h1>Personal Information</h1>
-            <form onSubmit={enviado}>
+            {!mostrarInfo ? (
+                <form onSubmit={enviado}>
+                    <div>
+                        <label htmlFor="first_name">Fisrt Name:</label>
+                        <input
+                            type="text"
+                            id="first_name"
+                            name="name"
+                            value={datos.name}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="second_name">Second Name: </label>
+                        <input
+                            type="text"
+                            id="second_name"
+                            name="lastname"
+                            value={datos.lastname}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="phone">Phone: </label>
+                        <input
+                            type="tel"
+                            id="phone"
+                            name="phone"
+                            value={datos.phone}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="correo">Email: </label>
+                        <input
+                            type="email"
+                            id="correo"
+                            name="email"
+                            value={datos.email}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+            ) : (
                 <div>
-                    <label htmlFor="first_name">Fisrt Name:</label>
-                    <input
-                        type="text"
-                        id="first_name"
-                        name="name"
-                        value={datos.name}
-                        onChange={handleChange}
-                    />
+                    <h2>Submitted Data</h2>
+                    <p>
+                        <strong>First Name:</strong> {datos.name}
+                    </p>
+                    <p>
+                        <strong>Second Name:</strong> {datos.lastname}
+                    </p>
+                    <p>
+                        <strong>Phone:</strong> {datos.phone}
+                    </p>
+                    <p>
+                        <strong>Email:</strong> {datos.email}
+                    </p>
+
+                    <button onClick={handleEditar}>Editar</button>
                 </div>
-                <div>
-                    <label htmlFor="second_name">Second Name: </label>
-                    <input
-                        type="text"
-                        id="second_name"
-                        name="lastname"
-                        value={datos.lastname}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="phone">Phone: </label>
-                    <input
-                        type="tel"
-                        id="phone"
-                        name="phone"
-                        value={datos.phone}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="correo">Email: </label>
-                    <input
-                        type="email"
-                        id="correo"
-                        name="email"
-                        value={datos.email}
-                        onChange={handleChange}
-                    />
-                </div>
-                <button type="submit">Submit</button>
-            </form>
+            )}
         </div>
     );
 }
