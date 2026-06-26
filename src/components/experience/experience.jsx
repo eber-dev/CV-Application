10c44fa6dc1e9b49f4a762aec9b139c4d61b9c87
Eber <www.eberacunaquispe@gmail.com>
2026-06-25 22:30:34 -0500

    Se añadio el cambio de estado al formulario de la seccion de experiencias

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -10,6 +10,7 @@ function Experience() {
 
     const enviado = (e) => {
         e.preventDefault();
+        setMostrarInfo(true);
     };
 
     const cambio = (e) => {
@@ -21,42 +22,70 @@ function Experience() {
         });
     };
 
+    const [mostrarInfo, setMostrarInfo] = useState(false);
+
+    const handleEditar = () => {
+        setMostrarInfo(false);
+    };
+
     return (
         <div>
             <h1>Educational Experience</h1>
-            <form onSubmit={enviado}>
+            {!mostrarInfo ? (
+                <form onSubmit={enviado}>
+                    <div>
+                        <label htmlFor="institucion_name">
+                            Intitucion Name:{' '}
+                        </label>
+                        <input
+                            type="text"
+                            name="institucion"
+                            id="institucion_name"
+                            value={datos.institucion}
+                            onChange={cambio}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="grado">Degree:</label>
+                        <input
+                            type="text"
+                            name="degree"
+                            id="grado"
+                            value={datos.degree}
+                            onChange={cambio}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="graduation">Graduation Date: </label>
+                        <input
+                            type="date"
+                            name="graduation_date"
+                            id="graduation"
+                            value={datos.graduation_date}
+                            onChange={cambio}
+                        />
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+            ) : (
                 <div>
-                    <label htmlFor="institucion_name">Intitucion Name: </label>
-                    <input
-                        type="text"
-                        name="institucion"
-                        id="institucion_name"
-                        value={datos.institucion}
-                        onChange={cambio}
-                    />
+                    <h2>Educational Experience</h2>
+                    <p>
+                        <strong>Intitucion Name:</strong>
+                        {datos.institucion}
+                    </p>
+                    <p>
+                        <strong>Degree:</strong>
+                        {datos.degree}
+                    </p>
+                    <p>
+                        <strong>Graduation Date:</strong>
+                        {datos.graduation_date}
+                    </p>
+
+                    <button onClick={handleEditar}>Editar</button>
                 </div>
-                <div>
-                    <label htmlFor="grado">Degree:</label>
-                    <input
-                        type="text"
-                        name="degree"
-                        id="grado"
-                        value={datos.degree}
-                        onChange={cambio}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="graduation">Graduation Date: </label>
-                    <input
-                        type="date"
-                        name="graduation_date"
-                        id="graduation"
-                        value={datos.graduation_date}
-                        onChange={cambio}
-                    />
-                </div>
-                <button type="submit">Submit</button>
-            </form>
+            )}
         </div>
     );
 }
