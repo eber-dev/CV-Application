a7738e472975db9de042190b9563ce6d1f269d6e
Eber <www.eberacunaquispe@gmail.com>
2026-06-26 19:48:35 -0500

    Se añadio el formulario con el renderizado condicional asimismo el valor de los datos se alamacena en el estado para evitar que react vacie los campos del formulario

--- src/components/practice/practice.jsx
+++ src/components/practice/practice.jsx
@@ -6,75 +6,116 @@ function Practice() {
         company: '',
         posicion: '',
         responsabilities: '',
+        start: '',
+        finish: '',
     });
 
     const enviado = (e) => {
         e.preventDefault();
+        setMostrarInfo(true);
     };
 
     const cambio = (e) => {
         const { name, value } = e.target;
-
         setDatos({
             ...datos,
             [name]: value,
         });
     };
 
+    const [mostrarInfo, setMostrarInfo] = useState(false);
+
+    const handleEditar = () => {
+        setMostrarInfo(false);
+    };
+
     return (
         <div>
             <h1>Practical Experience</h1>
-            <form onSubmit={enviado}>
-                <div>
-                    <label htmlFor="company">Company Name: </label>
-                    <input
-                        type="text"
-                        name="company"
-                        id="company"
-                        onChange={cambio}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="posicion">Posicion Title: </label>
-                    <input
-                        type="text"
-                        name="posicion"
-                        id="posicion"
-                        onChange={cambio}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="responsabilities">
-                        Main Responsabilities:
-                    </label>
-                    <input
-                        type="text"
-                        name="responsabilities"
-                        id="responsabilities"
-                        onChange={cambio}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="start">Start Employment</label>
-                    <input
-                        type="text"
-                        name="start"
-                        id="start"
-                        onChange={cambio}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="finish">Finish Employment</label>
-                    <input
-                        type="text"
-                        name="finish"
-                        id="finish"
-                        onChange={cambio}
-                    />
-                </div>
+            {!mostrarInfo ? (
+                <form onSubmit={enviado}>
+                    <div>
+                        <label htmlFor="company">Company Name: </label>
+                        <input
+                            type="text"
+                            name="company"
+                            id="company"
+                            value={datos.company}
+                            onChange={cambio}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="posicion">Posicion Title: </label>
+                        <input
+                            type="text"
+                            name="posicion"
+                            id="posicion"
+                            value={datos.posicion}
+                            onChange={cambio}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="responsabilities">
+                            Main Responsabilities:
+                        </label>
+                        <input
+                            type="text"
+                            name="responsabilities"
+                            id="responsabilities"
+                            value={datos.responsabilities}
+                            onChange={cambio}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="start">Start Employment</label>
+                        <input
+                            type="text"
+                            name="start"
+                            id="start"
+                            value={datos.start}
+                            onChange={cambio}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="finish">Finish Employment</label>
+                        <input
+                            type="text"
+                            name="finish"
+                            id="finish"
+                            value={datos.finish}
+                            onChange={cambio}
+                        />
+                    </div>
 
-                <button type="submit">Submit</button>
-            </form>
+                    <button type="submit">Submit</button>
+                </form>
+            ) : (
+                <div>
+                    <h2>Practice</h2>
+                    <p>
+                        <strong>Company Name:</strong>
+                        {datos.company}
+                    </p>
+                    <p>
+                        <strong>Posicion Title:</strong>
+                        {datos.posicion}
+                    </p>
+                    <p>
+                        <strong>Main Responsabilities:</strong>
+                        {datos.responsabilities}
+                    </p>
+                    <p>
+                        <strong>Start Employment:</strong>
+                        {datos.start}
+                    </p>
+                    <p>
+                        <strong>Finish Employment: </strong>
+                        {datos.finish}
+                    </p>
+
+                    <button onClick={handleEditar}>Editar</button>
+                </div>
+            )}
         </div>
     );
 }
