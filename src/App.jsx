f9760ec80f3a6690ce71149d2d7fe4f878a8815c
Eber <www.eberacunaquispe@gmail.com>
2026-06-30 16:29:59 -0500

    Se añaden los componentes de las 3 secciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Seccion1 from './components/general/seccion1.jsx';
 import Experience from './components/experience/experience.jsx';
 import Practice from './components/practice/practice.jsx';
+import './App.css';
 
 function App() {
     return (
-        <>
+        <div className="principal">
             <Seccion1 />
             <Experience />
             <Practice />
-        </>
+        </div>
     );
 }
 
